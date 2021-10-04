9c4366fecf6de6d31bb8dcb1f706006e54374c14
Dan Orzechowski <orzechod@gmail.com>
2021-10-04 19:34:28 -0400

    add typings to remote-data hooks

--- packages/frontend/public/@types/react.d.ts
+++ packages/frontend/public/@types/react.d.ts
@@ -1,19 +1,4 @@
 declare module 'react' {
-  interface ExoticComponent<P = {}> {
-    (props: P): (any | null);
-    readonly $$typeof: symbol;
-  }
-
-  interface NamedExoticComponent<P = {}> extends ExoticComponent<P> {
-    displayName?: string | undefined;
-  }
-
-  interface ProviderExoticComponent<P> extends ExoticComponent<P> {
-    propTypes?: undefined;
-  }
-
-  interface ProviderProps<T> {
-    value: T;
-    children?: undefined;
-  }
+  interface ProviderExoticComponent<P> { }
+  interface ProviderProps<T> { }
 }
