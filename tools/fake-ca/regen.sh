043c96847a9bb8f2db9c12adbce532e2b35727c9
Dan Orzechowski <orzechod@gmail.com>
2022-01-09 15:08:30 -0500

    root CA updates

--- tools/fake-ca/regen.sh
+++ tools/fake-ca/regen.sh
@@ -21,7 +21,6 @@ openssl req -config openssl.cnf -key key.pem -new -x509 -days 3650 -sha256 -exte
 
 popd > /dev/null
 
-exit 0
 ####################
 # intermediate CA
 
@@ -30,13 +29,19 @@ pushd ca
 # configure directory
 touch index.txt
 echo 1001 > serial
+echo 1001 > crlnumber
 mkdir -p certs crl csr newcerts private
 
 # generate private key
-# generate CSR
-# generate certificate
+openssl genrsa -out key.pem 4096
 
-popd
+# generate CSR
+openssl req -config openssl.cnf -new -sha256 -key key.pem -out csr.pem
+
+# generate certificate
+popd > /dev/null
+pushd root-ca
+openssl ca -batch -config openssl.cnf -extensions v3_intermediate_ca -days 1825 -notext -md sha256 -in ../ca/csr.pem -out ../ca/crt.pem
 
 ####################
 # server
