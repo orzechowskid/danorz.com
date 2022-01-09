99fa5c207863682ba1a3125e79efa444bd484bdd
Dan Orzechowski <orzechod@gmail.com>
2022-01-09 18:35:28 -0500

    checkpoint

--- tools/fake-ca/regen.sh
+++ tools/fake-ca/regen.sh
@@ -43,11 +43,19 @@ popd > /dev/null
 pushd root-ca
 openssl ca -batch -config openssl.cnf -extensions v3_intermediate_ca -days 1825 -notext -md sha256 -in ../ca/csr.pem -out ../ca/crt.pem
 
+popd > /dev/null
+
 ####################
 # server
 
 mkdir -p domains/local.host
 
-# generate server private key
-# generate server CSR
-# generate server certificate
+# generate private key
+openssl genrsa -out domains/local.host/key.pem 2048
+
+# generate CSR
+openssl req -config openssl.cnf -new -sha256 -key domains/local.host/key.pem -out domains/local.host/csr.pem
+exit 0
+# generate certificate
+pushd ca
+openssl ca -batch -config openssl.cnf -extensions server_cert -days 720 -notext -md sha256 -in ../domains/local.host/csr.pem -out ../domains/local.host/crt.pem
