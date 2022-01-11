e9281a7400e0644d998be10df800a08ff6f0cca9
Dan Orzechowski <orzechod@gmail.com>
2022-01-10 19:02:36 -0500

    update openssl.cnf

--- tools/fake-ca/regen.sh
+++ tools/fake-ca/regen.sh
@@ -1,63 +1,22 @@
 #!/bin/bash -xe
 
 # configure directory
-mkdir -p root-ca ca domains
+mkdir -p ca domains/local.host
 
 ####################
-# root CA
+# CA
 
-pushd root-ca
-
-# configure directory
-touch index.txt
-echo 1001 > serial
-mkdir -p certs crl csr newcerts private
-
-# generate private key
-openssl genrsa -out key.pem 4096
-
-# generate certificate
-openssl req -config openssl.cnf -key key.pem -new -x509 -days 3650 -sha256 -extensions v3_ca -out crt.pem
-
-popd > /dev/null
-
-####################
-# intermediate CA
-
-pushd ca
-
-# configure directory
-touch index.txt
-echo 1001 > serial
-echo 1001 > crlnumber
-mkdir -p certs crl csr newcerts private
-
-# generate private key
-openssl genrsa -out key.pem 4096
-
-# generate CSR
-openssl req -config openssl.cnf -new -sha256 -key key.pem -out csr.pem
-
-# generate certificate
-popd > /dev/null
-pushd root-ca
-openssl ca -batch -config openssl.cnf -extensions v3_intermediate_ca -days 1825 -notext -md sha256 -in ../ca/csr.pem -out ../ca/crt.pem
-
-popd > /dev/null
+# generate private key and certificate
+openssl req -nodes -subj "/CN=local.host.ca/OU=Dev Team/O=Alewife Labs/L=Worcester/ST=MA/C=US" -new -x509 -days 3650 -keyout ca/key.pem -out ca/crt.pem
 
 ####################
 # server
-
-mkdir -p domains/local.host
 
 # generate private key
 openssl genrsa -out domains/local.host/key.pem 2048
 
 # generate CSR
-openssl req -config openssl.cnf -new -sha256 -key domains/local.host/key.pem -out domains/local.host/csr.pem
+openssl req -new -sha256 -nodes -subj "/CN=local.host/OU=Dev Team/O=Alewife Labs/L=Worcester/ST=MA/C=US" -extensions v3_req -reqexts SAN -config openssl.cnf -key domains/local.host/key.pem -out domains/local.host/csr.pem
 
 # generate certificate
-pushd ca
-openssl ca -batch -config openssl.cnf -extensions server_cert -days 720 -notext -md sha256 -in ../domains/local.host/csr.pem -out ../domains/local.host/crt.pem -subj "/CN=AlewifeLabsGateway"
-
-popd > /dev/null
+openssl x509 -req -days 720 -in domains/local.host/csr.pem -CA ca/crt.pem -CAkey ca/key.pem -CAcreateserial -out domains/local.host/crt.pem -extfile openssl.cnf -extensions SAN
