c0c5c4be25bf2efd773f91b662e039ee2b7dd121
Dan Orzechowski <orzechod@gmail.com>
2022-01-23 13:10:26 -0500

    rename some config files so linting works everywhere

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
+/* eslint-env node */
+
 module.exports = {
+  clearMocks: true,
   projects: [
     `<rootDir>/packages/*`
   ]
--- packages/frontend/public/utils/dataCache.test.js
+++ packages/frontend/public/utils/dataCache.test.js
@@ -4,34 +4,66 @@ import {
 
 describe(`the DataCache class`, function() {
   describe(`when manually invoked`, function() {
-    it(`sets and gets with the same key`, function() {
-      const cache = new DataCache();
+    /** @type {DataCache} */
+    let cache;
 
+    beforeEach(function() {
+      cache = new DataCache();
+    });
+
+    it(`sets and gets with the same key`, function() {
       cache.set(`my-key`, 123);
 
       const value = cache.get(`my-key`);
 
       expect(value.data).toEqual(123);
     });
+
+    it(`respects the TTL argument of set()`, function() {
+      jest.useFakeTimers();
+
+      const ttl = 1000;
+
+      cache.set(`my-key`, 123, ttl);
+      jest.advanceTimersByTime(1001);
+      const data = cache.get(`my-key`);
+
+      expect(data).toEqual(undefined);
+    });
+
+    it(`clears correctly`, function() {
+      cache.set(`my-key`, 123);
+      cache.clear(`my-key`);
+      const data = cache.get(`my-key`);
+
+      expect(data).toEqual(undefined);
+    });
   });
 
   describe(`when listening for updates`, function() {
-    const listener = jest.fn();
     const key = `my-key`;
+    /** @type {DataCache} */
     let cache;
 
     beforeEach(function() {
       cache = new DataCache();
-      cache.on(key, listener);
-    });
-
-    afterEach(function() {
-      cache.off(key, listener);
     });
 
     it(`fires an event when a cache update is made at the given key`, function() {
+      const listener = jest.fn();
+
+      cache.on(key, listener);
       cache.set(key, 123);
       expect(listener).toHaveBeenCalledTimes(1);
+    });
+
+    it(`doesn't fire an event if a listener has been removed`, function() {
+      const listener = jest.fn();
+
+      cache.on(key, listener);
+      cache.off(key, listener);
+      cache.set(key, 123);
+      expect(listener).not.toHaveBeenCalled();
     });
   });
 });
