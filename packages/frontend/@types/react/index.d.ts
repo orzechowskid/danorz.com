a65b013056890966c8851514e8e0aa3b1f97f4f4
Dan Orzechowski <orzechod@gmail.com>
2022-01-23 13:47:28 -0500

    Chore/jest (#53)
    * initial test
    
    * more test tooling updates
    
    * rename some config files so linting works everywhere
    
    * cleanup

--- packages/frontend/@types/react/index.d.ts
+++
@@ -1,9 +0,0 @@
-/* this is just to make swr happy */
-
-declare module 'react' {
-  import {
-    FunctionComponent
-  } from 'preact';
-
-  export type FC<T> = FunctionComponent<T>;
-}
--- packages/frontend/public/utils/dataCache.test.js
+++ packages/frontend/public/utils/dataCache.test.js
@@ -1,7 +1,69 @@
-/* eslint-env jest */
+import {
+  DataCache
+} from './dataCache.js';
 
 describe(`the DataCache class`, function() {
-  it(`sets and gets with the same key`, function() {
-    expect(true).toBe(true);
+  describe(`when manually invoked`, function() {
+    /** @type {DataCache} */
+    let cache;
+
+    beforeEach(function() {
+      cache = new DataCache();
+    });
+
+    it(`sets and gets with the same key`, function() {
+      cache.set(`my-key`, 123);
+
+      const value = cache.get(`my-key`);
+
+      expect(value.data).toEqual(123);
+    });
+
+    it(`respects the TTL argument of set()`, function() {
+      jest.useFakeTimers();
+
+      const ttl = 1000;
+
+      cache.set(`my-key`, 123, ttl);
+      jest.advanceTimersByTime(1001);
+      const data = cache.get(`my-key`);
+
+      expect(data).toEqual(undefined);
+    });
+
+    it(`clears correctly`, function() {
+      cache.set(`my-key`, 123);
+      cache.clear(`my-key`);
+      const data = cache.get(`my-key`);
+
+      expect(data).toEqual(undefined);
+    });
+  });
+
+  describe(`when listening for updates`, function() {
+    const key = `my-key`;
+    /** @type {DataCache} */
+    let cache;
+
+    beforeEach(function() {
+      cache = new DataCache();
+    });
+
+    it(`fires an event when a cache update is made at the given key`, function() {
+      const listener = jest.fn();
+
+      cache.on(key, listener);
+      cache.set(key, 123);
+      expect(listener).toHaveBeenCalledTimes(1);
+    });
+
+    it(`doesn't fire an event if a listener has been removed`, function() {
+      const listener = jest.fn();
+
+      cache.on(key, listener);
+      cache.off(key, listener);
+      cache.set(key, 123);
+      expect(listener).not.toHaveBeenCalled();
+    });
   });
 });
